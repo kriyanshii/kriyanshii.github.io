aa577ad2911508036b30142763261281c401da6b
kriyanshii <kriyanshishah06@gmail.com>
2025-10-28 12:34:32 +0530

    remoded /portfoio

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // GitHub Pages base path
+  // base path removed for root deployment
   build: {
     outDir: 'dist',
     rollupOptions: {
